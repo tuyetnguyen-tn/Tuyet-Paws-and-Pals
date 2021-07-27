90bb2c5765aaa1bd355b862b84bfd2ab6d15ab84
Scott T <scott.tessm@gmail.com>
2021-07-26 20:33:08 -0500

    Rearranged some files/folders and changed up paths of things after merged. Readded models.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,12 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -8,6 +16,33 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -15,3 +50,5 @@ User.init(
     modelName: 'user',
   }
 );
+
+module.exports = User;
